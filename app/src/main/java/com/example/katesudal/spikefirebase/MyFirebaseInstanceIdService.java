603df873c0f291cbd6cbe5fd6913a2f02166d267
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-14 18:09:11 +0700

    Edit firebase notification

--- app/src/main/java/com/example/katesudal/spikefirebase/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/katesudal/spikefirebase/MyFirebaseInstanceIdService.java
@@ -1,10 +1,27 @@
 package com.example.katesudal.spikefirebase;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 /**
  * Created by katesuda.l on 14/12/2559.
  */
 
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FirebaseInstance:";
+    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage" ;
+
+    @Override
+    public void onTokenRefresh() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "FCM Token: " + token);
+
+        // Once a token is generated, we subscribe to topic.
+//        FirebaseMessaging.getInstance()
+//                .subscribeToTopic(FRIENDLY_ENGAGE_TOPIC);
+    }
 }
