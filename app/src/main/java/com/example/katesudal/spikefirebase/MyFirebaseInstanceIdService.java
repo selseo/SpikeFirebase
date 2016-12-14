90ac89916cb23d75b98df3185504cd768a99f7a2
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-14 16:30:46 +0700

    Add firebaseMessagingService and firebaseInstanceIdService

---
+++ app/src/main/java/com/example/katesudal/spikefirebase/MyFirebaseInstanceIdService.java
@@ -0,0 +1,10 @@
+package com.example.katesudal.spikefirebase;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by katesuda.l on 14/12/2559.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/example/katesudal/spikefirebase/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.example.katesudal.spikefirebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by katesuda.l on 14/12/2559.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
