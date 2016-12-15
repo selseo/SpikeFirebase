25fda2aeed4e53ceca8832bd20eb93cb4569b975
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-15 18:38:01 +0700

    Add request and response log

--- app/src/main/java/com/example/katesudal/spikefirebase/MainActivity.java
+++ app/src/main/java/com/example/katesudal/spikefirebase/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.katesudal.spikefirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DatabaseReference mDatabase;
+    Button buttonSendNoti;
+    EditText editTextSendNotiMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +35,58 @@ public class MainActivity extends AppCompatActivity {
         mDatabase.child("message").setValue("test");
         mDatabase.child("message2").setValue("test2");
 
+        buttonSendNoti = (Button) findViewById(R.id.buttonSendNoti);
+        editTextSendNotiMessage = (EditText) findViewById(R.id.editTextSendNotiMessage);
+
+        buttonSendNoti.setOnClickListener(this);
+
+//        Log.d("Token=", FirebaseInstanceId.getInstance().getToken());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonSendNoti) {
+            Log.d("Respone=","Request clivk");
+            String notiMessage = String.valueOf(editTextSendNotiMessage.getText());
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(createHttLoggingInterceptor())
+                    .build();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .client(client)
+                    .baseUrl("https://fcm.googleapis.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            NotificationCallAPI api = retrofit.create(NotificationCallAPI.class);
+
+            SendNotiRequest sendNotiRequest = new SendNotiRequest();
+            Notification notification = new Notification("New Notification", notiMessage);
+            sendNotiRequest.setNotification(notification);
+            sendNotiRequest.setTo(FirebaseInstanceId.getInstance().getToken());
+//            sendNotiRequest.setTo("doQtcVavX08:APA91bGSQNm2jEgnefPOYsEaO1trFhMhl0ls6MALDwMfrUKtwEj6BdTtfdFM4SY2UFArbgXXjrleKIcez8Gnq-uuuYBl9_lFnTJjNR5--S91sbvwErGKj31x892GNGPBbc0LZMKkz1Vg");
+
+            Call<SendNotiResponse> call = api.postNotification(sendNotiRequest);
+            call.enqueue(new Callback<SendNotiResponse>() {
+                @Override
+                public void onResponse(Call<SendNotiResponse> call, Response<SendNotiResponse> response) {
+                    Log.d("Respone=","Request Success");
+
+                }
+
+                @Override
+                public void onFailure(Call<SendNotiResponse> call, Throwable t) {
+                    Log.d("Respone=","Request error",t);
+                }
+            });
+
+        }
+    }
+
+    private HttpLoggingInterceptor createHttLoggingInterceptor() {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return httpLoggingInterceptor;
     }
 }
